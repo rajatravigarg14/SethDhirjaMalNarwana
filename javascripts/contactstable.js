d074968d2f593cc67adceee8463288a8eb2e285c
Rajat Ravi Garg <rajatravigarg14@gmail.com>
2018-07-14 20:40:00 +0530

    First commit of seth dhirja mal website

---
+++ javascripts/contactstable.js
@@ -0,0 +1,581 @@
+var contactslist = {
+    "contacts": [
+        {
+            "name": "Mohan Prakash Gupta,Advocate",
+            "contact": "9812220946"
+        },
+        {
+            "name": "Ajay Gupta S/O Om Parkash Gupta",
+            "contact": "9466013817"
+        },
+        {
+            "name": "Virender Kumar Garg S/O Lajpat Rai",
+            "contact": "9992746046"
+        },
+        {
+            "name": "Birender Garg S/O Beerchand Garg",
+            "contact": "9255415984"
+        },
+        {
+            "name": "Balraj Garg",
+            "contact": "9467685613"
+        },
+        {
+            "name": "Ravi Bhushan Garg,Advocate",
+            "contact": "9466320940"
+        },
+        {
+            "name": "Gourav Ravi Garg S/O Ravi Bhushan",
+            "contact": "8950542204"
+        },
+        {
+            "name": "Rajat Ravi Garg S/O Ravi Bhushan",
+            "contact": "8950405195"
+        },
+        {
+            "name": "Puneet Garg S/O Virender Kumar Garg ",
+            "contact": "9717194139"
+        },
+        {
+            "name": "Sumeet Garg S/O Virender Kumar Garg",
+            "contact": "7838804761"
+        },
+        {
+            "name": "Anil Kumar S/O Nathu Lal",
+            "contact": "9416174748"
+        },
+        {
+            "name": "Pankaj S/O Jagan Nath",
+            "contact": "9416060157"
+        },
+        {
+            "name": "Sanjay Garg S/O Rajender Pal",
+            "contact": "9416178644"
+        },
+        {
+            "name": "Vinod Kumar S/O Ishwar Das ",
+            "contact": "9416546247"
+        },
+        {
+            "name": "Rishi Garg S/O Tarsem Aggarawl",
+            "contact": "9812042252"
+        },
+        {
+            "name": "Anil Kumar S/O Jaswant Rai Garg",
+            "contact": "8950040023"
+        },
+        {
+            "name": "Parmod Garg S/O Girish Pal Garg",
+            "contact": "9416061727"
+        },
+        {
+            "name": "Anil Kumar S/O Tek Chand",
+            "contact": "9355443152"
+        },
+        {
+            "name": "Rakesh Garg S/O Jagdish Garg",
+            "contact": "9416284497"
+        },
+        {
+            "name": "Vinod Gupta, Vinod Hospital",
+            "contact": "9416948675"
+        },
+        {
+            "name": "Rajesh Gupta S/O Bharat Bhushan Gupta",
+            "contact": "9416268414"
+        },
+        {
+            "name": "Jeewan Parkash S/O Kulwant Rai",
+            "contact": "9416664553"
+        },
+        {
+            "name": "Ranbeer Chand Garg",
+            "contact": "9416226678"
+        },
+        {
+            "name": "Sandeep S/O Surender Kumar Gupta",
+            "contact": "9416555929"
+        },
+        {
+            "name": "Narender S/O Ranbeer Garg",
+            "contact": "9416226678"
+        },
+        {
+            "name": "Sudhir Garg S/O Lal Chand",
+            "contact": "9812052128"
+        },
+        {
+            "name": "Priyavarat S/O Tara Chand",
+            "contact": "7876380444"
+        },
+        {
+            "name": "Kaku Ram S/O Balwant Rai",
+            "contact": "9416558977"
+        },
+        {
+            "name": "Naresh Garg S/O Dalip Chand",
+            "contact": "9416463713"
+        },
+        {
+            "name": "Vipin S/O Beer Bhan",
+            "contact": "9416558406"
+        },
+        {
+            "name": "Satish S/O Beer Bhan",
+            "contact": "9728414544"
+        },
+        {
+            "name": "Krishan Kumar S/O Mangat Ram",
+            "contact": "9416254001"
+        },
+        {
+            "name": "Parveen S/O Beer Bhan",
+            "contact": "9416227270"
+        },
+        {
+            "name": "Parveen Gupta S/O Ved Parkash",
+            "contact": "9812202704"
+        },
+        {
+            "name": "Rajiv Gupta S/O Sat Prakash",
+            "contact": "9416178548"
+        },
+        {
+            "name": "Raj Kumar S/O Bansi Lal",
+            "contact": "9416502825"
+        },
+        {
+            "name": "Krishan Gopal S/O Sita Ram",
+            "contact": "9315359602"
+        },
+        {
+            "name": "Mahavir Garg S/O Kishan Chand",
+            "contact": "9416339324"
+        },
+        {
+            "name": "Vinod Gupta S/O Nathu Lal",
+            "contact": "9466734845"
+        },
+        {
+            "name": "Sanjay Garg S/O Ramesh Garg",
+            "contact": "9813966330"
+        },
+        {
+            "name": "Arun Kuman S/O Padam Parkash",
+            "contact": "9467239239"
+        },
+        {
+            "name": "Kusum Gupta S/O Beer Bhan",
+            "contact": "9417054558"
+        },
+        {
+            "name": "Vikas Pal Gupta S/O Mahinder Pal",
+            "contact": ""
+        },
+        {
+            "name": "Shravan Gupta S/O Om Parkash Gupta",
+            "contact": "9514102972"
+        },
+        {
+            "name": "Narayan Parsad Gupta S/O B.B. Gupta",
+            "contact": "9814011847"
+        },
+        {
+            "name": "Ashwani Gupta S/O Y.P. Gupta",
+            "contact": "9814166166"
+        },
+        {
+            "name": "Puneet S/O Narender Pal",
+            "contact": "904171800"
+        },
+        {
+            "name": "Jitender Aggarwal S/O Narender Pal",
+            "contact": "9988446160"
+        },
+        {
+            "name": "Sanjay Garg S/O Krishan Gopal",
+            "contact": "9356045670"
+        },
+        {
+            "name": "Sushil Garg S/O Puran Mal",
+            "contact": "9216548541"
+        },
+        {
+            "name": "Bharat Bhushan S/O Om Parkash",
+            "contact": "9803564445"
+        },
+        {
+            "name": "Subhash Garg S/O Om Parkash",
+            "contact": "9803564445"
+        },
+        {
+            "name": "Swatantra Gupta S/O Om PArkash",
+            "contact": "9888512351"
+        },
+        {
+            "name": "Shagun Gupta S/O Parveen Gupta",
+            "contact": "9988968706"
+        },
+        {
+            "name": "Chakravarti Garg S/O Seeta Ram",
+            "contact": "9463189579"
+        },
+        {
+            "name": "Raman Garg S/O Jagdish Garg",
+            "contact": "9417656222"
+        },
+        {
+            "name": "Pawan Gupta S/O Beer Bhan",
+            "contact": ""
+        },
+        {
+            "name": "Anil Gupta /S/O Jagdish Rai",
+            "contact": "9216713854"
+        },
+        {
+            "name": "Pardeep Gupta S/O Jaswant Rai",
+            "contact": "9216713854"
+        },
+        {
+            "name": "Ravinder Garg S/O Gyan Chand",
+            "contact": "9466847255"
+        },
+        {
+            "name": "Ashok Gupta S/O Girdhari Lal",
+            "contact": ""
+        },
+        {
+            "name": "Kapeesh Gupta S/O Bharat Bhushan Gupta",
+            "contact": "9501004499"
+        },
+        {
+            "name": "Ashok Gupta S/O Om Parkash",
+            "contact": "8699720895"
+        },
+        {
+            "name": "Smt. Manju W/O Late Subhash Gupta",
+            "contact": "0172-2583454"
+        },
+        {
+            "name": "Sanjeev Gupta S/O C.R. Gupta",
+            "contact": "9814908123"
+        },
+        {
+            "name": "Jivtesh Gupta S/O Kusum Gupta",
+            "contact": ""
+        },
+        {
+            "name": "Ishwar Parkash Gupta",
+            "contact": "9312940831"
+        },
+        {
+            "name": "Rakesh Gupta S/O Ishwar Gupta",
+            "contact": "9810122355"
+        },
+        {
+            "name": "Surender Gupta ",
+            "contact": "9899980706"
+        },
+        {
+            "name": "Ugarsen S/O Om Parkash",
+            "contact": "9910744653"
+        },
+        {
+            "name": "Ashok Gupta S/O Shyam Lal Gupta",
+            "contact": "9717370172"
+        },
+        {
+            "name": "Kailash Chand Garg S/O Puranmal Garg",
+            "contact": "9818536222"
+        },
+        {
+            "name": "Parveen Garg S/O K.C.Garg",
+            "contact": "9810039884"
+        },
+        {
+            "name": "Ashok Garg",
+            "contact": "9818151565"
+        },
+        {
+            "name": "Sudarshan Kumar S/O Puranmal Garg",
+            "contact": "8800116796"
+        },
+        {
+            "name": "Shravan Garg",
+            "contact": "9312795193"
+        },
+        {
+            "name": "Ashok Garg",
+            "contact": "9811162274"
+        },
+        {
+            "name": "Pawan Gupta",
+            "contact": "9212574369"
+        },
+        {
+            "name": "Mahender Garg",
+            "contact": "9999771782"
+        },
+        {
+            "name": "Vijay Mohan",
+            "contact": "9416108085"
+        },
+        {
+            "name": "Ranbir Garg S/O Kishan Chand",
+            "contact": "9416560034"
+        },
+        {
+            "name": "Wazir Singh Garg S/O Kishan Chand",
+            "contact": "9213646581"
+        },
+        {
+            "name": "Suresh Gupta",
+            "contact": "9817897005"
+        },
+        {
+            "name": "Vinod Gupta(Babli)",
+            "contact": "9310383561"
+        },
+        {
+            "name": "Satish Gupta",
+            "contact": "9416772958"
+        },
+        {
+            "name": "Balbir Garg S/O Kishan Chand",
+            "contact": "9466752684"
+        },
+        {
+            "name": "Vinod Gupta",
+            "contact": "9215332558"
+        },
+        {
+            "name": "Rajiv Gupta S/O CR Gupta",
+            "contact": "9415017331"
+        },
+        {
+            "name": "Joginder Garg S/O Kishan Chand",
+            "contact": "9416210647"
+        },
+        {
+            "name": "Anil Gupta S/O Lajpat Rai Gupta",
+            "contact": "9416388393"
+        },
+        {
+            "name": "Sushil Gupta S/O LR Gupta",
+            "contact": "9416388346"
+        },
+        {
+            "name": "Dinesh Gupta S/O LR Gupta",
+            "contact": "9416254054"
+        },
+        {
+            "name": "Prem S/O Sita Ram Master",
+            "contact": ""
+        },
+        {
+            "name": "Lal Chand",
+            "contact": ""
+        },
+        {
+            "name": "Parbodh Kumar Gupta S/O Lal Chand Patwani",
+            "contact": "8950891844"
+        },
+        {
+            "name": "Raj Kumar Garg S/O Master Deena Nath",
+            "contact": "9958856282"
+        },
+        {
+            "name": "Dr Rajesg Garg S/O Ram Kumar Garg",
+            "contact": "9996130222"
+        },
+        {
+            "name": "Ashok Garg S/O Master Nathu Lal",
+            "contact": "01686-263737"
+        },
+        {
+            "name": "T.C. Aggarwal",
+            "contact": "9416831752"
+        },
+        {
+            "name": "Surender Aggarwal S/O Balbir Garg",
+            "contact": "9253042336"
+        },
+        {
+            "name": "Sushila Gupta",
+            "contact": "9355570227"
+        },
+        {
+            "name": "Balraj Bedi",
+            "contact": "9467685613"
+        },
+        {
+            "name": "Tirlok Chand Gupta",
+            "contact": ""
+        },
+        {
+            "name": "Sunil Prakash S/O Kulwant Rai",
+            "contact": "9541933433"
+        },
+        {
+            "name": "Mukesh Gupta S/O Ishwar Prakash Gupta",
+            "contact": ""
+        },
+        {
+            "name": "Dinesh Gupta S/O Ishwar Prakash Gupta",
+            "contact": ""
+        },
+        {
+            "name": "Satyavarat Gupta S/O Tara Chand",
+            "contact": "8447774440"
+        },
+        {
+            "name": "Suman Bhushan Garg",
+            "contact": "9891122888"
+        },
+        {
+            "name": "Dharmpal Gupta",
+            "contact": "9871053678"
+        },
+        {
+            "name": "Satpal",
+            "contact": "9313884733"
+        },
+        {
+            "name": "Balwant Rai",
+            "contact": ""
+        },
+        {
+            "name": "Ram Kumar Garg S/O Prithvi Chand",
+            "contact": "9810718857"
+        },
+        {
+            "name": "Ramesh Garg",
+            "contact": "9958015637"
+        },
+        {
+            "name": "Suresh Garg",
+            "contact": "9310727223"
+        },
+        {
+            "name": "Adarsh Kumar Gupta S/O Suresh Gupta",
+            "contact": "9312375100"
+        },
+        {
+            "name": "Pankaj Mohan S/O Suresh Garg",
+            "contact": "9811392215"
+        },
+        {
+            "name": "GR Kishor S/O Suresh Garg",
+            "contact": "9871357515"
+        },
+        {
+            "name": "Dharampal Gupta S/O Kishan chand",
+            "contact": "9811137989"
+        },
+        {
+            "name": "Naveen Gupta Judge S/O Sateesh Gupta",
+            "contact": "9971993816"
+        },
+        {
+            "name": "Balwant Rai Gupta S/O Lala Bakshi Ram",
+            "contact": "9212578515"
+        },
+        {
+            "name": "Sanjay Gupta S/O Balwant Rai Gupta",
+            "contact": "9212578515"
+        },
+        {
+            "name": "Pawan Gupta S/O Balwant Rai Gupta",
+            "contact": "9313830980"
+        },
+        {
+            "name": "Praveen Gupta S/O Devraj Gupta",
+            "contact": "9810128824"
+        },
+        {
+            "name": "Pradeep Gupta S/O Devraj Gupta",
+            "contact": "9810029336"
+        },
+        {
+            "name": "Balwant Rai Gupta S/O Tula Ram",
+            "contact": "011-28562982"
+        },
+        {
+            "name": "Sudhir Garg S/O Girish Pal Garg",
+            "contact": "9810166878"
+        },
+        {
+            "name": "Mahavir Gupta S/O PP Gupta",
+            "contact": "9811461121"
+        },
+        {
+            "name": "Pardeep Gupta S/O BP Gupta",
+            "contact": "9818342522"
+        },
+        {
+            "name": "Yudh Raj Gupta",
+            "contact": "9212006655"
+        },
+        {
+            "name": "Balram Gupta",
+            "contact": "9811344097"
+        },
+        {
+            "name": "Ramesh Garg",
+            "contact": "9654855671"
+        }
+    ]
+}
+
+//Comparer Function  
+function GetSortOrder(prop) {  
+    return function(a, b) {  
+        if (a[prop] > b[prop]) {  
+            return 1;  
+        } else if (a[prop] < b[prop]) {  
+            return -1;  
+        }  
+        return 0;  
+    }  
+}  
+
+contactslist.contacts.sort(GetSortOrder("name"));
+
+// Find a <table> element with id="contactstable":
+var table = document.getElementById("contactstable");
+
+for (var i = 0; i < contactslist.contacts.length; i++) {
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(i+1);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = i + 1;
+    cell2.innerHTML = contactslist.contacts[i].name;
+    cell3.innerHTML = contactslist.contacts[i].contact;
+}
+
+
+function searchFunction() {
+    // Declare variables 
+    var input, filter, tr, td, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 1; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
+    }
+  }
